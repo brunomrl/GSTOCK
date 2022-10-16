4afa3ef69eadf572cc2f90d97e48a9e9443ca10d
brunomrl <b.doamaral2015@gmail.com>
2022-10-16 20:03:55 -0300

    Estruturação do Layout

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,7 +5,11 @@ namespace App\Controllers;
 class Home extends BaseController
 {
     public function index()
+
     {
-        return view('welcome_message');
+        $data = [
+            'titulo' => 'Home'
+        ];
+        return view('Home/index', $data);
     }
 }
